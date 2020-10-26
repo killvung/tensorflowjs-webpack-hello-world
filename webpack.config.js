62a4a0248ea817a964e6747705117aa4fbc269ef
killvung <killvung@gmail.com>
2020-10-26 15:57:07 -0500

    remove :dev remove entry and output

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,6 @@ var path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'Hello World',
